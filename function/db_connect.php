1df777a5b2f5957538284c4f214e188ba4e5c781
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2020-06-22 20:23:43 +0700

    Update : deployment

--- function/db_connect.php
+++ function/db_connect.php
@@ -1,5 +1,5 @@
 <?php
 
-// $db = mysqli_connect('localhost', 'aplika35_cat', '*aplikasicat#', 'aplika35_cat');
+$db = mysqli_connect('localhost', 'aplika35_cat', '*aplikasicat#', 'aplika35_cat');
 //$db = mysqli_connect('sql208.epizy.com', 'epiz_25109606', 'BAqRTEkLWhaAt', 'epiz_25109606_cat_db');
-$db = mysqli_connect('localhost', 'root', '', 'cat_db');
+// $db = mysqli_connect('localhost', 'root', '', 'cat_db');
