c5152754c5e8239b22c69a58e2155b7837cc4a2c
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2020-07-16 21:11:30 +0700

    Update riwayat soal

--- peserta/riwayatSoal.php
+++ peserta/riwayatSoal.php
@@ -40,10 +40,11 @@ require '../layouts/sidebar.php'; ?>
                         <?php while ($dataRiwayat = mysqli_fetch_assoc($listRiwayatSoal)) { ?>
                             <div class="my-3">
                                 <?php
-                                $dataRiwayat['isi_soal'] = str_replace($dataRiwayat['jawab'] . ". ", "<b style='background:red; color:white'>" . $dataRiwayat['jawab'] . ". </b>", $dataRiwayat['isi_soal']);
-                                $dataRiwayat['isi_soal'] = str_replace($dataRiwayat['kunci_soal'] . ". ", "<b style='background:green; color:white'>" . $dataRiwayat['kunci_soal'] . ". </b>", $dataRiwayat['isi_soal']);
+                                $soal = nl2br($dataRiwayat['isi_soal']);
+                                $soal = str_replace($dataRiwayat['jawab'] . ". ", "<b style='background:red; color:white'>" . $dataRiwayat['jawab'] . ". </b>", $soal);
+                                $soal = str_replace($dataRiwayat['kunci_soal'] . ". ", "<b style='background:green; color:white'>" . $dataRiwayat['kunci_soal'] . ". </b>", $soal);
                                 ?>
-                                <?php echo $dataRiwayat['state'] + 1 . ". " . ltrim(nl2br($dataRiwayat['isi_soal']), '<p>'); ?>
+                                <?php echo $dataRiwayat['state'] + 1 . ". " . ltrim($soal, '<p>'); ?>
                                 <hr>
                             </div>
                         <?php } ?>
