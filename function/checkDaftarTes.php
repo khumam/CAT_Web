a50f55c563af17d9b4f4734120906dbfe88e8514
Khoerul Umam <khoerulumam@students.unnes.ac.id>
2020-07-30 20:43:16 +0700

    Update : hanya bisa tes utama ketika tes demo selesai

---
+++ function/checkDaftarTes.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'db_connect.php';
+
+$id_tes = 5;
+$id_user = $_SESSION['id'];
+$session_id = $_SESSION['session_id'];
+
+$sql = "SELECT * FROM cat_hasil WHERE id_tes = $id_tes AND id_user = $id_user AND session_id = '$session_id' LIMIT 1";
+$query = mysqli_query($db, $sql);
+
+$checkDaftarTes = mysqli_fetch_assoc($query);
